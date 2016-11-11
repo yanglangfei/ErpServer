38bdc4f812eff52dd36ea7447995853bb693c3ff
langfei yang <185601452@qq.com>
2016-11-11 11:27:28 +0800

    lst

--- Erp/src/com/yf/erp/base/AutoCreate.java
+++ Erp/src/com/yf/erp/base/AutoCreate.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AutoCreate extends HttpServlet {
-	
-	
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
