ce03ad9ca25471f90676043203f9788e20929da1
langfei yang <185601452@qq.com>
2017-01-06 17:08:03 +0800

    dwr push

--- Erp/src/com/yf/erp/util/PushUtil.java
+++ Erp/src/com/yf/erp/util/PushUtil.java
@@ -1,22 +1,34 @@
 package com.yf.erp.util;
-
 import java.util.Collection;
+import java.util.UUID;
+
 import org.directwebremoting.ScriptBuffer;
+import org.directwebremoting.ScriptSession;
 import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
 import org.directwebremoting.proxy.dwr.Util;
 public class PushUtil {
 	public void pushMessage(String msg){
-		System.out.println("pushMessage...");
+		//获取Context 对象
 		WebContext context=WebContextFactory.get();
+		//获取所有打开页面的session集合
 		Collection sessions = context.getAllScriptSessions();
+		ScriptSession session = context.getScriptSession();
+		String uId = (String) session.getAttribute("userId");
+		if(uId==null||"".equals(uId)){
+			session.setAttribute("userId", UUID.randomUUID().toString());
+		}
+		System.out.println("uId:"+session.getAttribute("userId"));
+		//创建script 脚本
 		ScriptBuffer sb=new ScriptBuffer();
+		//调用back方法，并传递msg参数
 		sb.appendScript("back(");
 		sb.appendData(msg);
 		sb.appendScript(")");
+		//以session集合创建util对象
 		Util util=new Util(sessions);
+		//推送消息
 		util.addScript(sb);
-		System.out.println("addSb:"+msg);
 	}
 
 }
